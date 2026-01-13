f4c419ab730a973459fc918a47ecb4158a83ad47
sabi <sabina.buta@gmail.com>
2026-01-13 14:56:50 +0200

    Reorganize function declarations and add move struct

--- checkmate.h
+++ checkmate.h
@@ -5,5 +5,8 @@ void run_tests();
 void decode(char table[][8], char* fen);
 int solve(char table[][8], char tomove, int mode, FILE *out);
 int is_attacked(char table[][8], char color);
-
+typedef struct{
+    char c1,c2;
+    int r1,r2;
+}move;
 #endif // CHECKMATE_H_INCLUDED

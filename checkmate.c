d104bca2e866bd166ab2a53abd67f3f9f59ab74a
sabi <sabina.buta@gmail.com>
2026-01-13 14:56:32 +0200

    Refactor checkmate.c for improved readability

--- checkmate.c
+++ checkmate.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "checkmate.h"
 
 void decode(char table[][8], char* fen)
@@ -38,7 +39,7 @@ void decode(char table[][8], char* fen)
 
 int is_attacked(char table[][8], char tomove)
 {
-    int kr,kc;
+    int kr=-1, kc=-1;
     char king=(tomove=='w')?'K':'k';
     for(int i=0; i<8; i++)
         for(int j=0; j<8; j++)
@@ -53,15 +54,15 @@ int is_attacked(char table[][8], char tomove)
     char q_opp=(tomove=='w')?'q':'Q';
     char p_opp=(tomove=='w')?'p':'P';
     char k_opp=(tomove=='w')?'k':'K';
-    int ndr[]={2, 1, -1, -2, -2, -1, 1, 2};
-    int ndc[]={1, 2, 2, 1, -1, -2, -2, -1};
+    int ndr[]= {2, 1, -1, -2, -2, -1, 1, 2};
+    int ndc[]= {1, 2, 2, 1, -1, -2, -2, -1};
     for(int k=0; k<8; k++)
     {
         int r=kr+ndr[k], c=kc+ndc[k];
         if(r>=0 && r<8 && c>=0 && c<8 && table[r][c]==n_opp) return 1;
     }
-    int dr[]={-1, 1, 0, 0, -1, -1, 1, 1};
-    int dc[]={0, 0, -1, 1, -1, 1, -1, 1};
+    int dr[]= {-1, 1, 0, 0, -1, -1, 1, 1};
+    int dc[]= {0, 0, -1, 1, -1, 1, -1, 1};
     for(int k=0; k<8; k++)
     {
         for(int s=1; s<8; s++)
@@ -71,7 +72,7 @@ int is_attacked(char table[][8], char tomove)
             char p=table[r][c];
             if(p!=' ')
             {
-                if(p==q_opp) return 1;
+                if(p==q_opp) return 1;//queens attack from any direction
                 if(k<4 && p==r_opp) return 1;
                 if(k>=4 && p==b_opp) return 1;
                 if(s==1 && p==k_opp) return 1;
@@ -79,7 +80,7 @@ int is_attacked(char table[][8], char tomove)
             }
         }
     }
-    int pd=(tomove=='w')?1:-1;
+    int pd=(tomove=='w')?1:-1;//enemy black pawns are above while enemy white pawns are below
     if(kr+pd>=0 && kr+pd<8)
     {
         if(kc-1>=0 && table[kr+pd][kc-1]==p_opp) return 1;
@@ -99,39 +100,31 @@ int solve(char table[][8], char tomove, int mode, FILE *out)
             if(p==' ') continue;
             if(tomove=='w' && (p<'A' || p>'Z')) continue;
             if(tomove=='b' && (p<'a' || p>'z')) continue;
-            int dr[]={-1, 1, 0, 0, -1, -1, 1, 1};
-            int dc[]={0, 0, -1, 1, -1, 1, -1, 1};
-            int n_dr[]={2, 1, -1, -2, -2, -1, 1, 2};
-            int n_dc[]={1, 2, 2, 1, -1, -2, -2, -1};
-            int *cur_dr=dr, *cur_dc=dc;
+            int dr[]= {-1, 1, 0, 0, -1, -1, 1, 1};
+            int dc[]= {0, 0, -1, 1, -1, 1, -1, 1};
+            int n_dr[]= {2, 1, -1, -2, -2, -1, 1, 2};
+            int n_dc[]= {1, 2, 2, 1, -1, -2, -2, -1};
+            int *current_dr=dr, *current_dc=dc;
             int start=0, end=8, steps=8;
             if(p=='N' || p=='n')
             {
-                cur_dr=n_dr;
-                cur_dc=n_dc;
+                current_dr=n_dr;
+                current_dc=n_dc;
                 steps=1;
             }
             else if(p=='K' || p=='k')
-            {
                 steps=1;
-            }
             else if(p=='R' || p=='r')
-            {
                 end=4;
-            }
             else if(p=='B' || p=='b')
-            {
                 start=4;
-            }
             else if(p=='P' || p=='p')
-            {
-                end=0;
-            }
+                end=0;//disable loop for pawns
             for(int d=start; d<end; d++)
             {
                 for(int s=1; s<=steps; s++)
                 {
-                    int nr=i+cur_dr[d]*s, nc=j+cur_dc[d]*s;
+                    int nr=i+current_dr[d]*s, nc=j+current_dc[d]*s;
                     if(nr<0 || nr>7 || nc<0 || nc>7) break;
                     char target=table[nr][nc];
                     if(tomove=='w' && target>='A' && target<='Z') break;
@@ -139,12 +132,17 @@ int solve(char table[][8], char tomove, int mode, FILE *out)
                     table[i][j]=' ';
                     table[nr][nc]=p;
                     int success=0;
-                    if(!is_attacked(table, tomove))
+                    if(!is_attacked(table, tomove))//checks if own king is safe
                     {
                         if(mode==0) success=1;
                         else if(is_attacked(table, opp) && !solve(table, opp, 0, out))
                         {
-                            fprintf(out, "%c%d-%c%d\n", j+'a', i+1, nc+'a', nr+1);
+                            move m;
+                            m.c1=j+'a';
+                            m.r1=i+1;
+                            m.c2=nc+'a';
+                            m.r2=nr+1;
+                            fprintf(out, "%c%d-%c%d\n", m.c1, m.r1, m.c2, m.r2);
                             success=1;
                         }
                     }
@@ -157,17 +155,26 @@ int solve(char table[][8], char tomove, int mode, FILE *out)
             if(p=='P' || p=='p')
             {
                 int pd=(tomove=='w')?1:-1;
-                if(i+pd>=0 && i+pd<8 && table[i+pd][j]==' ')
+                int start_rank=(tomove=='w')?1:6;
+                int max_steps=(i==start_rank)?2:1;
+                for(int step=1; step<=max_steps; step++)
                 {
+                    int nr=i+pd*step;
+                    if(nr<0 || nr>7 || table[nr][j]!=' ') break;
                     table[i][j]=' ';
-                    table[i+pd][j]=p;
+                    table[nr][j]=p;
                     int success=0;
                     if(!is_attacked(table, tomove))
                     {
                         if(mode==0) success=1;
                         else if(is_attacked(table, opp) && !solve(table, opp, 0, out))
                         {
-                            fprintf(out, "%c%d-%c%d\n", j+'a', i+1, j+'a', i+pd+1);
+                            move m;
+                            m.c1=j+'a';
+                            m.r1=i+1;
+                            m.c2=j+'a';
+                            m.r2=i+pd+1;
+                            fprintf(out, "%c%d-%c%d\n", m.c1, m.r1, m.c2, m.r2);
                             success=1;
                         }
                     }
@@ -191,7 +198,12 @@ int solve(char table[][8], char tomove, int mode, FILE *out)
                                 if(mode==0) success=1;
                                 else if(is_attacked(table, opp) && !solve(table, opp, 0, out))
                                 {
-                                    fprintf(out, "%c%d-%c%d\n", j+'a', i+1, j+dx+'a', i+pd+1);
+                                    move m;
+                                    m.c1=j+'a';
+                                    m.r1=i+1;
+                                    m.c2=j+dx+'a';
+                                    m.r2=i+pd+1;
+                                    fprintf(out, "%c%d-%c%d\n", m.c1, m.r1, m.c2, m.r2);
                                     success=1;
                                 }
                             }
